d34b7cc5e643922cffcb075241f161646820f834
ShootedBall <wjdtmdwls45@gmail.com>
2018-04-29 11:47:21 +0900

    I delete OnClick function in start_btn script

--- Script/StartBtn.cs
+++ Script/StartBtn.cs
@@ -10,4 +10,23 @@ public class StartBtn : MonoBehaviour {
         SceneManager.LoadScene("[first_scene]");
     }
 
+
+    public void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if(GetComponent<SPriteR>)
+
+
+
+
+        }
+        
+
+
+    }
+
 }
